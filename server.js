faaf514cc8f82cbe51b531421adcf3538eb80eb2
hashem badawi <hashem_badawi89@outlook.sa>
2025-07-17 11:50:38 +0300

    updates for registration operation

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Schemas
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  phoneNumber: { type: String, required: true, unique: true },
+  phoneNumber: { type: String, unique: true },
   password: { type: String, required: true },
   isVerified: { type: Boolean, default: false },
   verificationCode: String
