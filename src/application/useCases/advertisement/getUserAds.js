869920222ef92f3d2d82c1fb2496d42dc3d7448e
hashem badawi <hashem_badawi89@outlook.sa>
2025-08-05 12:40:57 +0300

    Add user details retrieval to getUserAds function

--- src/application/useCases/advertisement/getUserAds.js
+++ src/application/useCases/advertisement/getUserAds.js
@@ -1,7 +1,9 @@
 const AdRepository = require('../../../domain/repositories/adRepository');
+const UserRepository = require('../../../domain/repositories/userRepository');
 
 const getUserAds = async (userId, page = 1, limit = 5) => {
   const { ads, total } = await AdRepository.findByUserId(userId, page, limit);
+  const user = await UserRepository.findById(userId);
   const mappedAds = ads.map(p => ({
     ...p,
     images: [p.pic1, p.pic2, p.pic3, p.pic4, p.pic5, p.pic6].filter(Boolean),
@@ -12,6 +14,12 @@ const getUserAds = async (userId, page = 1, limit = 5) => {
     page: parseInt(page),
     limit: parseInt(limit),
     ads: mappedAds,
+    user : {
+      userId: user._id,
+      phoneNumber: user.phoneNumber,
+      name: user.firstName + ' ' + user.lastName,
+      profilePicture: user.profileImage,
+    }
   };
 };
 
