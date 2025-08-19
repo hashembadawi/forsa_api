f606a119dd65a628aaaf9fa18230fa0b354cb0b4
hashem badawi <hashem_badawi89@outlook.sa>
2025-08-19 13:06:39 +0300

    Refactor getUserFavoriteAds to streamline ad mapping and remove redundant image deletions

--- src/application/useCases/favorites/getUserFavoriteAds.js
+++ src/application/useCases/favorites/getUserFavoriteAds.js
@@ -5,22 +5,31 @@ const getUserFavoriteAds = async (userId, page = 1, limit = 10) => {
     const { favorites, total } = await userFavoriteAdsRepository.getUserFavorites(userId, page, limit);
     
     // Map the favorites to include ad details and format images
-    const mappedFavorites = favorites.map(favorite => ({
-      favoriteId: favorite._id,
-      addedDate: favorite.addedDate,
-      ad: {
+    const mappedFavorites = favorites.map(favorite => {
+      const ad = favorite.adId ? {
         ...favorite.adId,
-        images: [favorite.adId.pic1, favorite.adId.pic2, favorite.adId.pic3, favorite.adId.pic4, favorite.adId.pic5, favorite.adId.pic6].filter(Boolean)
+        images: [
+          favorite.adId.pic1,
+          favorite.adId.pic2,
+          favorite.adId.pic3,
+          favorite.adId.pic4,
+          favorite.adId.pic5,
+          favorite.adId.pic6
+        ].filter(Boolean)
+      } : null;
+      if (ad) {
+        delete ad.pic1;
+        delete ad.pic2;
+        delete ad.pic3;
+        delete ad.pic4;
+        delete ad.pic5;
+        delete ad.pic6;
       }
-    }));
-    //remove the pic1, pic2, pic3, pic4, pic5, pic6 from mappedFavorites
-    mappedFavorites.forEach(fav => {
-      delete fav.ad.pic1;
-      delete fav.ad.pic2;
-      delete fav.ad.pic3;
-      delete fav.ad.pic4;
-      delete fav.ad.pic5;
-      delete fav.ad.pic6;
+      return {
+        favoriteId: favorite._id,
+        addedDate: favorite.addedDate,
+        ad
+      };
     });
     return {
       total,
